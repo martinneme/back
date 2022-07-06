1db36a73b76e9533691fd4f5d67973bf2ff8659c
mneme07 <mjneme1995@gmail.com>
2022-07-05 21:51:54 -0300

    update

--- userClass.js
+++ userClass.js
@@ -1,6 +1,7 @@
 
+const { Console } = require("console");
 const fs = require("fs");
-const { type } = require("os");
+
 
 
 class Contenedor{
@@ -9,53 +10,115 @@ class Contenedor{
   }
 
  async save(obj){
-  const content = await this.readFile()
+  try{
+    const content = await this.readFile()
    const cont = await JSON.parse(content)
    cont.length ? obj.id = await this.lastId(): obj.id=1;
    cont.push(obj)
    this.writeFile(JSON.stringify(cont))
+  }catch(e){
+    
+   console.error(e)
+  }
+  
 }
 
 async lastId(){
-  const content = await this.readFile()
+  try{
+   const content = await this.readFile()
    const cont = await JSON.parse(content)
+   if(!cont.length){
+    return 1
+   }
    const ultReg = cont.pop()
-    return ultReg.id+1
+    return ultReg.id+1 
+  }catch(e){
+    console.error(e)
+  }
+  
   }
 
+  async showGetByID(id){
+    try{
+     if(await this.readFile()){
+         console.log(await this.getById(id))
+    } 
+    }catch(e){
+      console.log(e)
+    }
+    
+ 
+  } 
 
   async getById(id){
-    const content = await this.readFile()
+    try{  
+       const content = await this.readFile()
      const cont = await JSON.parse(content)
- const reg=  cont.filter(reg=>reg.id===id)
 
-      return reg.length ? reg : null;
+    const reg=  cont.filter(reg=>reg.id===id)
+    return reg.length ? [ ...reg] : null; 
+    }catch(e){
+      if (e instanceof TypeError) {
+        throw e
+      }else{
+        console.error(e)
+      }
+
+      }
+ 
     }
 
 async getAll(){
-  const content = await this.readFile()
+  try{
+      const content = await this.readFile()
    const cont = await JSON.parse(content)
    console.log(cont)
+  }catch(e){
+    console.error(e)
+  }
+
 }
 
-deleteAll(){
-  this.writeFile(JSON.stringify([]))
+async deleteAll(){
+  try{
+        if(await this.readFile()){
+                this.writeFile(JSON.stringify([]))
+        }
+     
+  }catch(e){
+   if(e.errno === -4058){
+   console.log("Archivo no encontrado")
+   }
+   console.log(e)
+  }
+
 }
 
 
  async readFile(){
   return await fs.promises
     .readFile(this.pathFile, "utf-8")
-    .catch((err) => {
-      console.error(err);
+    .catch((e) => {
+      if (e.code === 'ENOENT') {
+        throw "Archivo no encontrado"
+      }
+      if(e instanceof TypeError){
+          throw "TypeError - Es posible que el archivo que intenta abrir no corresponda a la codificacion esperada";
+          }else {
+        throw "err";
+      }
     });
 }
 
 async writeFile(obj){
   return await fs.promises
     .writeFile(this.pathFile, obj)
-    .catch((err) => {
-      console.error(err);
+    .catch((e) => {
+      if (e.code === 'ENOENT' ) {
+        throw 'File not found!';
+      }else{
+        throw e;
+      }
     });
 }
 
@@ -65,8 +128,9 @@ async appendContentFile(content){
    .then(() => {
      console.log("agregado");
    })
-   .catch((err) => {
-     console.error(err);
+   .catch((e) => {
+     console.error(e);
+     console.log(`code error test:${e.code}`)
    });
 }
 
@@ -74,13 +138,41 @@ async appendContentFile(content){
 
 
 
-const test= new Contenedor("contenido.json");
+const test= new Contenedor("contenido.txt");
+
 const obj = { title: 'AddTitleTest', price: 63500, thumbnail: 'http://link4' }
 
-// test.save(obj)
+
+//   test.getById(3)
+//   test.showGetByID(1)
 //  test.getAll()
 // test.deleteAll()
-// test.lastId()
- test.getById(2).then(res=>{
-  console.log(res)
- })
+
+console.log(`1- Agregar un objeto al archivo
+2-Mostrar un elemento por ID
+3- Mostrar todos los elementos
+4- Eliminar todos los elementos`);
+let input;
+
+process.stdin.on('data',function(data){
+  input = parseInt(data.toString())
+  switch(input){
+    case 1:
+      test.save(obj);
+      break;
+    case 2:
+      test.showGetByID(3);
+      break;
+    case 3:
+      test.getAll();
+      break;
+    case 4:
+      test.deleteAll()
+      break
+  }
+  
+})
+
+
+
+
